a06d847508ad9c3bbf76ec3e5b077aab20d256e5
saurav B <146224541+sauravatg@users.noreply.github.com>
2024-07-24 05:52:42 -0400

    Add files via upload
    1. open terminal 
    2.then type  " bash basic.sh "

---
+++ basic.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt update;sudo apt install build-essential; 
+sudo apt install default-jdk;sudo apt install default-jre;
+sudo apt install python3;
+sudo snap install --classic code;
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb;sudo dpkg -i google-chrome-stable_current_amd64.deb;
